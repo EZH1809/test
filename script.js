9a04785fe4a15b5864e97f2ee81aa93c4c3f5bb8
EZH <stellage.ua@gmail.com>
2024-08-16 19:39:06 +0300

    add task

--- script.js
+++ script.js
@@ -1,51 +1,133 @@
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-  b = prompt("На сколько оцените его?", ""),
-  c = prompt("Один из последних просмотренных фильмов?", ""),
-  d = prompt("На сколько оцените его?", "");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
-
-// if (4 == 9) {
-//   console.log("ok");
-// } else {
-//   console.log("error");
+console.log("Hello, World!");
+// let num = 50;
+// while (num < 55) {
+//   console.log(num);
+//   num++;
 // }
 
-const num = 50;
-// if (num < 49) {
-//   console.log("Error");
-// } else if (num > 100) {
-//   console.log("Много");
-// } else {
-//   console.log("Все ОК");
+// for (let i = 10; i < 15; i++) {
+//   if (i === 13) {
+//     break;
+//   }
+//   console.log(i);
 // }
-// console.log(num === 50 ? "yes" : "no");
 
-// num === 50 ? console.log("yes") : console.log("no");
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+//   for (let j = 0; j < 10; j++) {
+//     console.log(j);
+//   }
+// }
 
-switch (num) {
-  case 49:
-    console.log("Неверно");
-    break;
-  case 100:
-    console.log("Неверно");
-    break;
-  case 50:
-    console.log("В точку");
-    break;
-  default:
-    console.log("Не в этот раз");
-    break;
+//*
+//**
+//***
+//****
+//*****
+//******
+// let result = "";
+// const lenght = 8;
+// for (let i = 1; i < lenght; i++) {
+//   for (let j = 0; j < i; j++) {
+//     result += "*";
+//   }
+//   result += "\n";
+// }
+// console.log(result);
+
+// let result = "";
+// const lenght = 5;
+
+// first: for (let i = 1; i < lenght; i++) {
+//   console.log(`First level: ${i}`);
+//   for (let j = 0; j < i; j++) {
+//     console.log(`Second level: ${j}`);
+//     for (let k = 0; k < j; k++) {
+//       if (k === 2) continue first;
+//       console.log(`Third level: ${k}`);
+//     }
+//     // result += "*";
+//   }
+//   // result += "\n";
+// }
+// console.log(result);
+
+// function firstTask() {
+//   for (let num = 5; num <= 10; num++) {
+//     console.log(`${num}`);
+//   }
+// }
+// firstTask();
+
+// function firstTask() {
+//   let num = 5;
+
+//   while (num <= 10) {
+//     console.log(`${num}`);
+//     num++;
+//   }
+// }
+// firstTask();
+
+// function secondTask() {
+//   for (let num2 = 20; num2 >= 10; num2--) {
+//     if (num2 === 13) break;
+//     console.log(`${num2}`);
+//   }
+// }
+// secondTask();
+
+// function thirdTask() {
+//   for (let num3 = 2; num3 <= 10; num3 += 2) {
+//     console.log(`${num3}`);
+//   }
+// }
+// thirdTask();
+
+// for (let i = 2; i <= 16; i++) {
+//   if (i % 2 === 0) {
+//     continue;
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// let i = 2;
+// while (i <= 16) {
+//   if (i % 2 === 0) {
+//     i++;
+//     continue;
+//   } else {
+//     console.log(i);
+//     i++;
+//   }
+// }
+
+function fifthTask() {
+  const arrayOfNumbers = [];
+
+  for (let i = 5; i <= 10; i++) {
+    arrayOfNumbers[i - 5] = i;
+  }
+
+  console.log(fifthTask());
+  return arrayOfNumbers;
 }
+fifthTask();
+/*
+  function fifthTask() {
+    const arrayOfNumbers = [];
+
+    for (let i = 5; i <= 10; i++) {
+        arrayOfNumbers[i - 5] = i;
+    }
+
+    // Не трогаем
+    return arrayOfNumbers;
+}
+
+console.log(fifthTask()); // [5, 6, 7, 8, 9, 10]
+
+  */
