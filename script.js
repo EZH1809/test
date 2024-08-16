39790ef69671eb5e5a81293272ed50b003ac8287
EZH <stellage.ua@gmail.com>
2024-08-16 21:25:58 +0300

    add code

--- script.js
+++ script.js
@@ -105,29 +105,62 @@ console.log("Hello, World!");
 //   }
 // }
 
-function fifthTask() {
-  const arrayOfNumbers = [];
+// function fifthTask() {
+//   const arrayOfNumbers = [];
 
-  for (let i = 5; i <= 10; i++) {
-    arrayOfNumbers[i - 5] = i;
+//   for (let i = 5; i <= 10; i++) {
+//     arrayOfNumbers[i - 5] = i;
+//   }
+
+//   console.log(fifthTask());
+//   return arrayOfNumbers;
+// }
+// fifthTask();
+/*
+Заполните новый массив (result) числами из старого (arr). Количество элементов в массиве можно получить как arr.length, а к элементам обращаемся все так же: arr[0], arr[1] и тд. */
+
+// function firstTask() {
+//   const arr = [3, 5, 8, 16, 20, 23, 50];
+//   const result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     result[i] = arr[i];
+//   }
+//   return result;
+// }
+// firstTask();
+
+// Измените данный массив так, чтобы все числа были увеличены в 2 раза, а если попадается строка строка - то к ней было добавлено " - done".
+// Для определения типа данных используйте typeof();
+// Должно получиться: [ 10, 20, 'Shopping - done', 40, 'Homework - done' ]
+
+// function secondTask() {
+//   // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//   const data = [5, 10, "Shopping", 20, "Homework"];
+//   for (let i = 0; i < data.length; i++) {
+//     if (typeof data[i] === "number") {
+//       data[i] *= 2;
+//     } else {
+//       data[i] += " - done";
+//     }
+//   }
+//   console.log(data);
+//   return data;
+// }
+// secondTask();
+
+// Разверните массив data наоборот при помощи цикла и запишите данные в массив result.
+// Должно получиться: [ 'Homework', 20, 'Shopping', 10, 5 ]
+
+function thirdTask() {
+  const data = [5, 10, "Shopping", 20, "Homework"];
+  const result = [];
+
+  for (let i = data.length - 1; i >= 0; i--) {
+    result[data.length - 1 - i] = data[i];
   }
 
-  console.log(fifthTask());
-  return arrayOfNumbers;
-}
-fifthTask();
-/*
-  function fifthTask() {
-    const arrayOfNumbers = [];
-
-    for (let i = 5; i <= 10; i++) {
-        arrayOfNumbers[i - 5] = i;
-    }
-
-    // Не трогаем
-    return arrayOfNumbers;
+  console.log(result);
+  return result;
 }
 
-console.log(fifthTask()); // [5, 6, 7, 8, 9, 10]
-
-  */
+thirdTask();
