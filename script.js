d21c9a1e2746fcf43f95da715a84d887ddf5b9cc
EZH1809 <145156906+EZH1809@users.noreply.github.com>
2024-08-22 13:06:22 +0300

    add code

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
 'use strict';
-
-const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-};
-const a = prompt ('Один из последних просмотренных фильмов?', ''),
-        b = prompt ('На сколько оцените его?', ''),
-        c = prompt ('Один из последних просмотренных фильмов?', ''),
-        d = prompt ('На сколько оцените его?', '');
-
-        personalMovieDB.movies[a]=b;
-        personalMovieDB.movies[c]=d;
-        console.log(personalMovieDB);
+document.getElementById("range").addEventListener("input", (e) => {
+    document
+      .querySelector(".graph-container")
+      .style.setProperty("--rating", e.target.value);
+  });
+  
